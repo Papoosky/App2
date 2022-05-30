0906e55e14a54f152682bdc264aeecd6de806b86
Itallo Ortega <101406650+Itall00@users.noreply.github.com>
2022-05-30 16:58:28 -0400

    ajuste buscar

---
+++ src/main/java/com/uai/app/ui/BuscarLibroUI.java
@@ -0,0 +1,62 @@
+package com.uai.app.ui;
+
+import com.uai.app.dominio.enums.Tittles;
+import com.uai.app.logic.SearchManager;
+import com.uai.app.ui.utils.UAIJFrame;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class BuscarLibroUI extends UAIJFrame {
+    private JPanel BuscarLibro;
+    private JLabel Encabezado;
+    private JButton buscarLibroButton;
+    private JTextField Cuadrotextobuscarlibro;
+    private JTextPane textPane1;
+    private JTextField sedetit;
+    private JButton buscarLibroButton1;
+    private JTextPane textPane2;
+
+    private JPanel mainPanel;
+    private JPanel mainTableConatiner;
+    private JTextArea textArea1;
+
+    public BuscarLibroUI(String title) {
+        super(title);
+        this.setMainPanel(BuscarLibro);
+        buscarLibroButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String tit = Cuadrotextobuscarlibro.getText();
+                SearchManager.getInstance().buscarlibroportitulo(Tittles.TITULO, tit);
+                String x = String.valueOf(SearchManager.getInstance().buscarlibroportitulo(Tittles.TITULO, tit));
+                /* si se encuentra el libro en x se guardan los datos del libro */
+
+                textPane1.setText(x);
+
+                //dispose();//cierra la ventana
+
+            }
+
+        });
+
+        buscarLibroButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String sedet = sedetit.getText();
+                SearchManager.getInstance().buscarlibroportitulo(Tittles.SEDE, sedet);
+                String y = String.valueOf(SearchManager.getInstance().buscarlibroportitulo(Tittles.SEDE, sedet));
+                //si encuentra alguna sede con ese nombre los guarda en y
+                textPane2.setText(y);
+
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
