d390699be56f6d249c9c6848701ae199f20720cd
Itoros <102181918+Itoros@users.noreply.github.com>
2022-05-30 00:44:54 -0400

    Delete App2-main-2 directory

--- App2-main-2/app/src/main/java/com/uai/app/exceptions/NotMainPanelLoadedException.java
+++
@@ -1,8 +0,0 @@
-package com.uai.app.exceptions;
-
-public class NotMainPanelLoadedException extends Exception {
-
-    public NotMainPanelLoadedException() {
-        super("No se especifico el panel principal del form");
-    }
-}
