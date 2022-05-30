39dd0e0e312534674f8a4d3955d2bdf902b4a57b
Pablo Zuñiga <80722868+Papoosky@users.noreply.github.com>
2022-05-30 19:40:14 -0400

    Delete appV1 directory

--- appV1/src/main/java/com/uai/app/exceptions/LibroNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.uai.app.exceptions;
-
-public class LibroNotFoundException extends Exception{
-    public LibroNotFoundException() {
-        super("El libro no se econtcontró");
-    }
-}
