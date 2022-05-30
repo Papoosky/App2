9f54245b6e6642d036c71f6e729655297f762fe0
Itoros <102181918+Itoros@users.noreply.github.com>
2022-05-30 00:43:35 -0400

    Delete app directory

--- app/src/main/java/com/uai/app/exceptions/DataNotLoadedException.java
+++
@@ -1,9 +0,0 @@
-package com.uai.app.exceptions;
-
-public class DataNotLoadedException extends Exception {
-
-    public DataNotLoadedException() {
-        super("La Data aun no se cargo antes de llamar a este metodo, " +
-                "debes llamar a setData antes");
-    }
-}
